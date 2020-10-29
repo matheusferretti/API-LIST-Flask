1e68b1e7a4f867298e96149473099f2955e282e1
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-29 00:58:35 +0000

    updating

--- src/app.py
+++ src/app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
-    return '<h1>Hello!</h1>'
+    return jsonify(todos)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
