0d661e00523a295b722b7ae8d6d9a323481ec1f6
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-29 20:18:18 +0000

    updating

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 todos = [
@@ -6,9 +6,12 @@ todos = [
     { "label": "My second task", "done": False }
 ]
 
-@app.route('/todos', methods=['GET'])
-def hello_world():
-    return jsonify(todos)
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    print("Incoming request with the following body", request_body)
+    return 'Response for the POST todo'
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
